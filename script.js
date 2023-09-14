a6c413d52e117512d26d1e215ebcae521b0824dd
Ingapee614 <132918785+Ingapee614@users.noreply.github.com>
2023-09-14 18:42:24 +0200

    challenge1-3

---
+++ script.js
@@ -0,0 +1,40 @@
+
+//CHALLENGE1
+
+/*This is the firstname of the user*/
+
+var user = 'john';
+
+
+/*This is the lastname of the user */
+ 
+var surname = 'smith';
+
+console.log(user,surname);
+
+
+//CHALLENGE2
+
+/** (c) ACME Inc. 2010 */
+
+/*
+ This is the date that a user created their account */
+ 
+ const d = new Date('10-07-2014')
+
+ console.log( d)
+
+//CHALLENGE3
+
+/*It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/
+const warning =" security scan starting";
+const info =" Please wait for scan to complete closing the browser";
+console.warn(warning);
+
+// It is important to let user know when they can close the page  
+
+console.info(info);
